23828e467d8e1f4cae919a550dd88793790f5325
pedro_mab <pedro_mab@outlook.com>
2025-10-07 18:21:55 -0300

    modified:   next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://localhost:5000/api/:path*',
-      },
-    ];
-  },
+  assetPrefix: 'hannah-figurinha-generator/'
 };
 
 export default nextConfig;
