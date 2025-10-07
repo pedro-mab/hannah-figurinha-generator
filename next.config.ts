6b09cb91360b8d67d5ae51879c9442f0f2e867c3
pedro_mab <pedro_mab@outlook.com>
2025-10-07 18:26:35 -0300

    modified:   next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: '/hannah-figurinha-generator',
-  assetPrefix: '/hannah-figurinha-generator',
 };
 
 export default nextConfig;
